075719cb26980623561b07b829a40c5ae424dfa8
Siddharth Aadarsh <siddharth.aadarsh@healthify.co>
2021-04-13 21:00:47 +0530

    web-app part 5

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-
-posts = [
-    {
-        'author': 'Siddharth',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 23, 2021'
-    },
-    {
-        'author': 'Aadarsh',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 24, 2021'
-    }
-] 
 # Create your views here.
 
 def Home(request):
